83c863139e88917ffd36fb93216dab5f1cb94e52
Solomon R Pena <solomonraulpena@gmail.com>
2019-08-28 05:45:14 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,42 @@
 def square_array(array)
-  # your code here
+squared_array = []
+i = 0
+while i < array.size do
+  squared_array<<array[i]**2
+i+=1
 end
+squared_array
+end
+
+
+
+
+
+# def square_array(array)
+# i = 0
+# squared_array = []
+# while i < array.size do
+#
+# i+=1
+# end
+# squared_array(numbers)
+# end
+
+
+
+#def square_array(array)
+# numbers = [1,2,3]
+#  squared_array = []
+#  i = 0
+#  while i < 0 do
+#  squared_array<<i**numbers
+#  i+=1
+#  end
+
+# counter = 0
+# pets = ["Dog", "Cat", "Fish", "Bird", "Hamster"]
+#
+# while pets[counter] do
+#   puts pets[counter]
+#   counter += 1
+# end
